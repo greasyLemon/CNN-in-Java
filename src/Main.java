7b959cdc9b16de762fc23fd62f24eb73d575fe08
lambodapho <lamtungob@gmail.com>
2024-04-08 10:07:54 +0700

    added Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,43 @@
 import data.DataReader;
 import data.Image;
+import network.NetworkBuilder;
+import network.NeuralNetwork;
 
 import java.util.List;
 
+import static java.util.Collections.shuffle;
+
 public class Main {
+
     public static void main(String[] args) {
-        //List<Image> img = new DataReader().readData();
-        //System.out.printf(img.get(0).toString());
+
+        long SEED = 123;
+
+        System.out.println("Starting data loading...");
+
+        List<Image> imagesTest = new DataReader().readData("data/mnist_test.csv");
+        List<Image> imagesTrain = new DataReader().readData("data/mnist_train.csv");
+
+        System.out.println("Images Train size: " + imagesTrain.size());
+        System.out.println("Images Test size: " + imagesTest.size());
+
+        NetworkBuilder builder = new NetworkBuilder(28, 28, 256*100);
+        builder.addConvolutionLayer(SEED, 5, 1, 0.1, 8);
+        builder.addMaxPoolLayer(2, 3);
+        builder.addFullyConnectedLayer(10, 0.1, SEED);
+
+        NeuralNetwork net = builder.build();
+
+        float rate = net.test(imagesTest);
+        System.out.println("Pre training success rate: " + rate);
+
+        int epochs = 3;
+
+        for(int i = 0; i < epochs; i++){
+            shuffle(imagesTrain);
+            net.train(imagesTrain);
+            rate = net.test(imagesTest);
+            System.out.println("Success rate after round " + i + ": " + rate);
+        }
     }
 }
--- src/network/NetworkBuilder.java
+++ src/network/NetworkBuilder.java
@@ -22,4 +22,32 @@ public class NetworkBuilder {
         _layers = new ArrayList<>();
     }
 
+    public void addConvolutionLayer(long SEED, int filterSize, int stepSize, double learningRate, int numFilters) {
+        if (_layers.isEmpty()) {
+            _layers.add(new ConvolutionLayer(SEED, filterSize, stepSize, 1, _inputRows, _inputCols, learningRate, numFilters));
+        } else {
+            Layer prev = _layers.getLast();
+            _layers.add(new ConvolutionLayer(SEED, filterSize, stepSize, prev.getOutputLength(), prev.getOutputRows(), prev.getOutputCols(), learningRate, numFilters));
+        }
+    }
+    public void addMaxPoolLayer(int stepSize, int windowSize) {
+        if (_layers.isEmpty()) {
+            _layers.add(new MaxPoolLayer(stepSize, windowSize, 1, _inputRows, _inputCols));
+        } else {
+            Layer prev = _layers.getLast();
+            _layers.add(new MaxPoolLayer(stepSize, windowSize, prev.getOutputLength(), prev.getOutputRows(), prev.getOutputCols()));
+        }
+    }
+    public void addFullyConnectedLayer(int outLength, double learningRate, long SEED) {
+        if (_layers.isEmpty()) {
+            _layers.add(new FullyConnectedLayer(SEED, _inputCols*_inputRows, outLength, learningRate));
+        } else {
+            Layer prev = _layers.getLast();
+            _layers.add(new FullyConnectedLayer(SEED, prev.getOutputElements(), outLength, learningRate));
+        }
+    }
+    public NeuralNetwork build(){
+        net = new NeuralNetwork(_layers, _scaleFactor);
+        return net;
+    }
 }
